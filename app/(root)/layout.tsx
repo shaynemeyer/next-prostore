2aad43b9860b2d911cf222bfe96ad593611000b2
Shayne Meyer <shayne.meyer@gmail.com>
2025-03-27 13:13:12 -0700

    initial commit

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className="flex h-screen flex-col">
+      <main className="flex-1 wrapper">{children}</main>
+    </div>
+  );
+}
+
+export default RootLayout;
