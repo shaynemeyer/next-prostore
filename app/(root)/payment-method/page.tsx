0d86e415638a560f16fc4cf4b83f24d011539723
Shayne Meyer <shayne.meyer@gmail.com>
2025-04-03 19:19:25 -0700

    added Payment method form

--- app/(root)/payment-method/page.tsx
+++ app/(root)/payment-method/page.tsx
@@ -1,5 +1,27 @@
-function PaymentMethodPage() {
-  return <div>PaymentMethodPage</div>;
+import { Metadata } from "next";
+import { auth } from "@/auth";
+import { getUserById } from "@/lib/actions/user.actions";
+import PaymentMethodForm from "./PaymentMethodForm";
+import CheckoutSteps from "@/components/shared/checkout-steps/CheckoutSteps";
+
+export const metadata: Metadata = {
+  title: "Select Payment Method",
+};
+
+async function PaymentMethodPage() {
+  const session = await auth();
+  const userId = session?.user?.id;
+
+  if (!userId) throw new Error("User not found");
+
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <CheckoutSteps current={2} />
+      <PaymentMethodForm preferredPaymentMethod={user.paymentMethod} />
+    </>
+  );
 }
 
 export default PaymentMethodPage;
---
+++ app/(root)/place-order/page.tsx
@@ -0,0 +1,4 @@
+function PlaceOrderPage() {
+  return <div>PlaceOrderPage</div>;
+}
+export default PlaceOrderPage;
