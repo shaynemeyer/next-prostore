f480d74ee814c1de41cee4f1c978968c7ab34167
Shayne Meyer <shayne.meyer@gmail.com>
2025-04-10 06:33:44 -0700

    added banner carousel component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,19 @@
+import ProductCarousel from "@/components/shared/product/ProductCarousel";
 import ProductList from "@/components/shared/product/ProductList";
-import { getLatestProducts } from "@/lib/actions/product.actions";
+import {
+  getLatestProducts,
+  getFeaturedProducts,
+} from "@/lib/actions/product.actions";
 
 async function Homepage() {
   const latestProducts = await getLatestProducts();
+  const featuredProducts = await getFeaturedProducts();
 
   return (
     <div>
+      {featuredProducts.length > 0 && (
+        <ProductCarousel data={featuredProducts} />
+      )}
       <ProductList data={latestProducts} title="Newest Arrivals" limit={4} />
     </div>
   );
